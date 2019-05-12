0c5bbb9cd9825f716988ac055fe8da62565ed7a5
dalyIsaac <isaac.daly@outlook.com>
2019-05-12 21:14:17 +1200

    Switched URL to local environment

--- src/common.ts
+++ src/common.ts
@@ -1,1 +1,2 @@
-export const baseUrl = "http://csse-s365.canterbury.ac.nz:4001/api/v1";
+// export const baseUrl = "http://csse-s365.canterbury.ac.nz:4001/api/v1";
+export const baseUrl = "http://localhost:4941/api/v1";
