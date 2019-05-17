54a8ef7778da723b16a286579f9466c9c187440c
dalyIsaac <isaac.daly@outlook.com>
2019-05-17 21:57:32 +1200

    Added buttons for editing and saving fields

--- src/views/Users/User.vue
+++ src/views/Users/User.vue
@@ -16,16 +16,29 @@
 
         <div v-if="email">
           <div class="tile">
+            <p class="subheading label">Email:</p>
+            <p class="subheading value">{{ email }}</p>
+          </div>
+
+          <v-divider/>
+
+          <div class="tile">
             <p class="subheading label">Given name:</p>
             <div class="value">
-              <v-text-field
-                v-model="givenName"
-                :rules="givenNameRules"
-                :counter="userMaximums.givenName"
-                label="Given name"
-                required
-                :disabled="!editingMode"
-              />
+              <v-layout row>
+                <v-btn flat icon color="primary" @click="editGivenName">
+                  <v-icon v-if="editingGivenName">save</v-icon>
+                  <v-icon v-else class="flipped-icon">edit</v-icon>
+                </v-btn>
+                <v-text-field
+                  v-model="givenName"
+                  :rules="givenNameRules"
+                  :counter="userMaximums.givenName"
+                  label="Given name"
+                  required
+                  :disabled="!editingGivenName"
+                />
+              </v-layout>
             </div>
           </div>
 
@@ -34,22 +47,63 @@
           <div class="tile">
             <p class="subheading label">Family name:</p>
             <div class="value">
-              <v-text-field
-                v-model="familyName"
-                :rules="familyNameRules"
-                :counter="userMaximums.givenName"
-                label="Family name"
-                required
-                :disabled="!editingMode"
-              />
+              <v-layout row>
+                <v-btn flat icon color="primary" @click="editFamilyName">
+                  <v-icon v-if="editingFamilyName">save</v-icon>
+                  <v-icon v-else class="flipped-icon">edit</v-icon>
+                </v-btn>
+                <v-text-field
+                  v-model="familyName"
+                  :rules="familyNameRules"
+                  :counter="userMaximums.givenName"
+                  label="Family name"
+                  required
+                  :disabled="!editingFamilyName"
+                />
+              </v-layout>
             </div>
           </div>
 
           <v-divider/>
 
           <div class="tile">
-            <p class="subheading label">Email:</p>
-            <p class="subheading value">{{ email }}</p>
+            <p class="subheading label">Password:</p>
+            <div class="value">
+              <v-layout row>
+                <v-btn :disabled="editingPassword" flat icon color="primary" @click="editPassword">
+                  <v-icon v-if="!editingPassword" class="flipped-icon">edit</v-icon>
+                </v-btn>
+                <v-text-field
+                  v-model="password"
+                  :rules="passwordRules"
+                  :counter="userMaximums.password"
+                  label="Password"
+                  required
+                  :disabled="!editingPassword"
+                />
+              </v-layout>
+            </div>
+          </div>
+
+          <v-divider/>
+
+          <div class="tile" v-if="editingPassword" transition="slide-x-transition">
+            <p class="subheading label">Password:</p>
+            <div class="value">
+              <v-layout row>
+                <v-btn v-if="editingPassword" flat icon color="primary" @click="editPassword">
+                  <v-icon>save</v-icon>
+                </v-btn>
+                <v-text-field
+                  v-model="confirmPassword"
+                  :rules="confirmPasswordRules"
+                  :counter="userMaximums.password"
+                  label="Confirm password"
+                  required
+                  :disabled="!editingPassword"
+                />
+              </v-layout>
+            </div>
           </div>
         </div>
 
@@ -87,11 +141,17 @@ export default Vue.extend({
     userMaximums,
     username: "",
     givenName: "",
+    editingGivenName: false,
     familyName: "",
+    editingFamilyName: false,
     email: "",
-    editingMode: false,
+    password: "",
+    confirmPassword: "",
+    editingPassword: false,
     givenNameRules,
-    familyNameRules
+    familyNameRules,
+    passwordRules: [],
+    confirmPasswordRules: []
   }),
   methods: {
     getUser() {
@@ -106,6 +166,15 @@ export default Vue.extend({
           // per the API spec.
           this.email = res.data.email;
         });
+    },
+    editGivenName() {
+      this.editingGivenName = !this.editingGivenName;
+    },
+    editFamilyName() {
+      this.editingFamilyName = !this.editingFamilyName;
+    },
+    editPassword() {
+      this.editingPassword = !this.editingPassword;
     }
   }
 });
@@ -116,6 +185,16 @@ export default Vue.extend({
   display: grid;
   grid-template-columns: 50% 50%;
   padding: 16px;
+}
+
+/* Credit to https://codepen.io/anon/pen/VqPRyp */
+.flipped-icon {
+  -moz-transform: scaleX(-1);
+  -o-transform: scaleX(-1);
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+  filter: FlipH;
+  -ms-filter: "FlipH";
 }
 
 .label {
