1ee279ad679ec6a4b0d1187b514eac6345629acb
dalyIsaac <isaac.daly@outlook.com>
2019-05-18 19:17:28 +1200

    Resolved #20 in a hackish sort of way

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>Hello world</div>
+  <div>
+    You really shouldn't be looking at this. Please stop and
+    <router-link to="/venues">go look at some venues</router-link>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  beforeMount() {
+    this.$router.push("/venues");
+  }
+});
 </script>
